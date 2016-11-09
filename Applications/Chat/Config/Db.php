fc43d57c2ab7c4967d4f1cf5bcd1caf0e70e395e
Finghin <okko1236987@gmail.com>
2016-11-09 10:08:09 +0800

    first commit

---
+++ Applications/Chat/Config/Db.php
@@ -0,0 +1,18 @@
+<?php
+namespace Config;
+/*
+ *mysql配置
+ *@author finghin
+ */
+class Db
+{
+    public static $db1 = array(
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'user' => 'root',
+        'password' => '123456',
+        'dbname' => 'wsc',
+        'charset' => 'utf8',
+    );
+}
+?>
